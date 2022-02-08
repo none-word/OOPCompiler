41b25f8cf678b301789ccf2eeb3d25fc425df9d2
gakostarev <gakostarev@mts.ru>
2022-02-09 01:07:22 +0300

    feat: syntax analyzer

--- src/main/java/com/example/compiler/syntaxer/GrammarChecker.java
+++ src/main/java/com/example/compiler/syntaxer/GrammarChecker.java
@@ -1,6 +1,7 @@
 package com.example.compiler.syntaxer;
 
 import com.example.compiler.lexer.Token;
+import com.example.compiler.lexer.TokenType;
 
 import java.util.List;
 
@@ -15,9 +16,27 @@ public class GrammarChecker {
         size = tokens.size();
     }
 
-    private void verifyToken(Token token, String lexeme) {
-        if (!token.getLexeme().equals(lexeme)) {
+    private String lexeme() {
+        return tokens.get(currentIndex).getLexeme();
+    }
+
+    private TokenType tokenType() {
+        return tokens.get(currentIndex).getType();
+    }
+
+    private void verifyToken(String lexeme) {
+        if (!lexeme().equals(lexeme)) {
             throw new CompilationException();
+        } else {
+            currentIndex++;
+        }
+    }
+
+    private void verifyTokenByTokenType(TokenType tokenType) {
+        if (!tokenType.equals(tokenType())) {
+            throw new CompilationException();
+        } else {
+            currentIndex++;
         }
     }
 
@@ -26,10 +45,71 @@ public class GrammarChecker {
 //
 //    }
 //
-//    public int getClassNameEndingIndex() {
-//
-//    }
+    public void specifyClassName() {
+        specifyIdentifier();
+        if (lexeme().equals("[")) {
+            currentIndex++;
+            specifyClassName();
+            if (!lexeme().equals("]")) {
+                throw new CompilationException();
+            } else {
+                currentIndex++;
+            }
+        }
+    }
 
+    public void specifyIdentifier() {
+        if (tokens.get(currentIndex).getType() != TokenType.IDENTIFIER) {
+            throw new CompilationException();
+        } else {
+            currentIndex++;
+        }
+    }
 
+    public void specifyMemberDeclaration() {
+        try {
+            specifyVariableDeclaration();
+        } catch (CompilationException e) {
+            try {
+                specifyMethodDeclaration();
+            } catch (CompilationException exception) {
+                specifyConstructorDeclaration();
+            }
+        }
+    }
 
+    public void specifyVariableDeclaration() {
+        verifyToken("var");
+        specifyIdentifier();
+        verifyToken(":");
+
+    }
+
+    public void specifyMethodDeclaration() {
+
+    }
+
+    public void specifyConstructorDeclaration() {
+
+    }
+
+    private void specifExpression() {
+    }
+
+    private void specifyArguments() {
+        verifyToken("(");
+
+    }
+
+    private void specifyPrimary() {
+        try {
+            verifyTokenByTokenType(TokenType.LITERAL);
+        } catch (Exception exception) {
+            try {
+                verifyToken("this");
+            } catch (Exception e) {
+                specifyClassName();
+            }
+        }
+    }
 }
